0e58b7d487a9d3f2087843d778ad73445e2cd31c
Heitor Magalhaes <viniciustrabalhos76@gmail.com>
2025-07-05 11:55:09 -0300

    coloquei jogadores, e adicionei a classe tecnicos

--- src/CodigosInternos/Jogadores.java
+++ src/CodigosInternos/Jogadores.java
@@ -1,4 +1,11 @@
 package CodigosInternos;
 
 public class Jogadores {
+
+    private String nome;
+    private int idade;
+    private String overall;
+    private String posicao;
+    private double valor;
+
 }
---
+++ src/CodigosInternos/Tecnicos.java
@@ -0,0 +1,4 @@
+package CodigosInternos;
+
+public class Tecnicos {
+}
