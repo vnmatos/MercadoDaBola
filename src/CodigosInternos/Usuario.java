7ebec3195f62d820be8d47a58dc131fcbaf1b48b
Heitor Magalhaes <viniciustrabalhos76@gmail.com>
2025-07-05 11:49:58 -0300

    inicio

---
+++ src/CodigosInternos/Usuario.java
@@ -0,0 +1,40 @@
+package CodigosInternos;
+
+public class Usuario {
+
+    private String nomePlayer;
+    private String nomeTime;
+    private double saldoBancario;
+
+
+    public String getNomePlayer() {
+        return nomePlayer;
+    }
+
+    public void setNomePlayer(String nomePlayer) {
+        this.nomePlayer = nomePlayer;
+    }
+
+    public String getNomeTime() {
+        return nomeTime;
+    }
+
+    public void setNomeTime(String nomeTime) {
+        this.nomeTime = nomeTime;
+    }
+
+    public double getSaldoBancario() {
+        return saldoBancario;
+    }
+
+    public void setSaldoBancario(double saldoBancario) {
+        this.saldoBancario = saldoBancario;
+    }
+
+
+
+
+
+
+
+}
